5e5a38764fc448c9cfddaa5372fc959a68d74a21
DavidEscudero <david.escudero.king@gmail.com>
2016-06-25 12:24:45 +0100

    added count and none to my enumerable method

--- MyEnumerable.rb
+++ MyEnumerable.rb
@@ -41,16 +41,58 @@ module Enumerable
 			end
 		end
 	end
+
+	def my_any
+		if block_given?
+			for i in 0..self.length-1
+				returnvar = false
+				if yield(self.to_a[i]) == true
+					returnvar = true
+					break
+				end
+			end
+			return returnvar
+		end
+	end
+
+	def my_none
+		if block_given?
+			returnvar = true
+			self.my_each do |i|
+				puts i
+				if yield(i) 
+					returnvar=false
+					break
+				end
+			end
+			return returnvar
+		end
+	end
+
+	def my_count(*p) #this will give a default argument of p=[] if no other arguments are given
+		count = 0
+		if block_given?
+			self.my_each {|i| count += 1 if yield(i) }
+		elsif p == []
+			self.my_each { count += 1 }
+		elsif p.length == 1
+			self.my_each { |i| count+=1 if i == p[0] }
+		end
+		return count
+	end
 end
 
 
-array = [1, 2, 3, 4, 5, 4, 2, 6]
+array = [1, 1, 3, 3, 3, 1, 4, 2, 1]
 #array.my_each_with_index {|el, index| puts el.to_s + ' ' + index.to_s}
 
-red = []
+#if array.my_none { |el|  el > 2 }
+#	puts 'true it is'
+#else
+#	puts 'false it be'
+#end
 
-if array.my_all { |el|  el < 8 }
-	puts 'true it is'
-else
-	puts 'false it be'
-end
+#counts the number of elements
+
+puts array.my_count {|el| el < 4}
+puts array.my_count
--- test.rb
+++ test.rb
@@ -8,3 +8,11 @@ red = array.select { |el|  el<=3 }
 puts red.to_s
 
 
+		if block_given?
+			for i in 0..self.length-1
+				if yield(self.to_a[i]) true
+					return true
+					break
+				end
+			end
+		end
