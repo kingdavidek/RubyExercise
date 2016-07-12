12f094fe3efa419e1f6589418ef0ed0f46062c60
DavidEscudero <david.escudero.king@gmail.com>
2016-07-12 16:32:23 +0200

    developing tictactoe game

--- tictactoe.rb
+++ tictactoe.rb
@@ -16,14 +16,16 @@ class TicTacToe
 	def show_board
 		puts "
 
-		#{@board[7]} | #{@board[0]}  | #{@board[0]}
+		#{@board[0]} | #{@board[1]}  | #{@board[2]}
 		-----------
-		#{@board[0]} | #{@board[0]}  | #{@board[0]}
+		#{@board[3]} | #{@board[4]}  | #{@board[5]}
 		-----------
-		#{@board[0]} | #{@board[0]}  | #{@board[0]}"
+		#{@board[6]} | #{@board[7]}  | #{@board[8]}"
 	end
 
 	def start_game 
+
+
 		x = true
 		#the turn mechanism works
 		until @winner do 
@@ -37,11 +39,30 @@ class TicTacToe
 
 			puts "player #{player} plays box # :"
 			box = gets.chomp.to_i
-			puts "1 Please input a valid box number between 0 and 8" unless box.is_a? Integer
-			puts "2 Please input a valid array" unless (box < 9 && box >= 0)
-			puts "cool"
-			
-			@winner = true
+			#turns box variable into an integer and sets box=0 if it can't do so
+			if box > 9 || box < 1
+				puts "Error: please input a valid box number between 0 and 9"
+				if x; x=false; else; x=true; end
+			elsif self.put_into_box(box, x) == false
+				puts "Error: please input a box number that is NOT taken."
+				if x; x=false; else; x=true; end
+			else
+				self.put_into_box(box, x)
+				self.show_board
+			end
+			#@winner = true
+		end
+	end
+
+	def put_into_box(num, x)
+		num -= 1
+		if @board[num] != nil
+			return false
+		end
+		if x
+			@board[num] = 'O'
+		else
+			@board[num] = 'X'
 		end
 	end
 
