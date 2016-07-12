08b6fd4d3f2d0ec6fb1fb90f93ba25508a6d6bf9
DavidEscudero <david.escudero.king@gmail.com>
2016-07-12 16:41:58 +0200

    made more DRY with 'turn' method

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,17 +1,12 @@
 
-
-
-
 class TicTacToe
 
 	def initialize
 		@board = [nil, nil, nil, nil, nil, nil, nil, nil, nil]
 		@winner = false
+		@x = true
+		@player = 'X'
 	end
-
-	#def board
-	#	puts @board
-	#end
 
 	def show_board
 		puts "
@@ -24,33 +19,33 @@ class TicTacToe
 	end
 
 	def start_game 
-
-
-		x = true
 		#the turn mechanism works
 		until @winner do 
-			if x
-				player = 'X'
-				x = false
-			elsif !x
-				player = 'O'
-				x = true
-			end
-
-			puts "player #{player} plays box # :"
+			self.turn
+			puts "player #{@player} plays box # :"
 			box = gets.chomp.to_i
 			#turns box variable into an integer and sets box=0 if it can't do so
 			if box > 9 || box < 1
 				puts "Error: please input a valid box number between 0 and 9"
-				if x; x=false; else; x=true; end
-			elsif self.put_into_box(box, x) == false
+				self.turn
+			elsif self.put_into_box(box, @x) == false
 				puts "Error: please input a box number that is NOT taken."
-				if x; x=false; else; x=true; end
+				self.turn
 			else
-				self.put_into_box(box, x)
+				self.put_into_box(box, @x)
 				self.show_board
 			end
 			#@winner = true
+		end
+	end
+
+	def turn
+		if @x
+			@player = 'X'
+			@x = false
+		elsif !@x
+			@player = 'O'
+			@x = true
 		end
 	end
 
@@ -78,6 +73,4 @@ end
 
 
 bum = TicTacToe.new
-
-bum.show_board
 bum.start_game
